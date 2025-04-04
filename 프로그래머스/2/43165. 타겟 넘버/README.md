7975d189c4448e578ba82d1bcbca21835825d937
Jaehyun <jhko@sogang.ac.kr>
2025-04-04 23:23:38 +0900

    [level 2] Title: 타겟 넘버, Time: 456.10 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.41 ms
+메모리: 9.21 MB, 시간: 456.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 0일 1:26:36
+2025년 04월 04일 23:23:36
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,22 @@
+from itertools import product
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
+
 def solution(numbers, target):
     answer = 0
-    leaves = [0]
-    for num in numbers:
-        tmp = []
-        for parent in leaves:
-            tmp.append(parent+num)
-            tmp.append(parent-num)
-        leaves=tmp.copy()
-    for leaf in leaves:
-        if leaf==target:
-            answer+=1
+
+    def dfs(index, current_sum):
+        nonlocal answer
+        # 모든 숫자를 다 사용했을 때
+        if index == len(numbers):
+            if current_sum == target:
+                answer += 1
+            return
+        # 현재 숫자를 더하거나 빼기
+        dfs(index + 1, current_sum + numbers[index])
+        dfs(index + 1, current_sum - numbers[index])
+    
+    dfs(0, 0)
     return answer
